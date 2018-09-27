bbc8272e09eaf40f6faa11f2857c4b401f5428a7
Weawer11qq <41298911+Weawer11qq@users.noreply.github.com>
2018-09-27 15:58:04 +0500

    Initial commit

---
+++ src/Fruit.java
@@ -0,0 +1,11 @@
+public abstract class Fruit {
+    protected double weight;
+
+    public Fruit(double weight) {
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+}
